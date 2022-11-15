3d78ae597f90a6383ce0da22571bbb049a829977
hjkang306 <hjkang306@gmail.com>
2022-11-15 19:39:53 +0900

    2022.11.15

--- vue_api/src/views/AboutView.vue
+++ vue_api/src/views/AboutView.vue
@@ -2,6 +2,97 @@
   <div>
     <HeaderCont />
     <TitleCont name1="About" name2="reference" />
+
+    <section className="cont__about">
+      <div className="container">
+        <div className="about__inner">
+          <div className="part1">
+            <img
+              src="https://images.unsplash.com/photo-1541959833400-049d37f98ccd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt="정글이미지"
+            />
+            <h3 className="about__title">나무를 보지 말고 숲을 보라.</h3>
+            <p>
+              나무를 보지 말고 숲을 보라. 이 말은 현재만 바라보는 좁은 시야를
+              벗어나 넓게 미래를 보라는 말처럼 느껴집니다. 제가 앞으로
+              이루어나갈 목표에 더욱 순수한 열망으로 감응하고, 즐겁게 미래를
+              상상할 줄 아는 인재로 살아가고 싶습니다. 어린 시절, 저는 보통의
+              범주와는 다소 거리가 있으며 당장의 삶이 힘겨울 때면 허무맹랑한
+              상상력을 발휘해 혼자만의 세계에 빠졌을 만큼 힘겨운 나날을
+              보냈지만, 그렇다고 해서 무조건 굴복하거나 무너지지 않고, 두 다리를
+              튼튼하게 지지하며 전진해 나가려는 노력을 기울이곤 했습니다. 타고난
+              환경이 유복한 사람이라면 스스로 의지를 꺾고 잠시 쉬어 가려고 해도
+              얼마든지 안락한 삶이 보장될 수 있지만, 저 같은 경우에는 스스로
+              나아가려는 의지조차 포기하는 순간부터 나락이 시작된다는 사실을
+              알고 있었기 때문입니다. 타인보다 벼랑을 훨씬 가까운 지척에 두고
+              살아가는 일은 때때로 저를 우울하게 했지만, 저는 그렇기 때문에 더욱
+              의욕을 북돋우며 끈기 있게 나아가, 저의 열정을 증명하며 밝은 미래로
+              나아갈 의지를 표명할 수 있어야 한다는 생각을 가졌던 것입니다.
+            </p>
+          </div>
+          <div className="part2">
+            <img
+              src="https://images.unsplash.com/photo-1525923838299-2312b60f6d69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"
+              alt="식물이미지"
+            />
+            <div>
+              <h3 className="about__title">
+                "인생 한 방" 보다는 작은 노력을 쌓아가는 사람
+              </h3>
+              <p>
+                식물은 잔잔하고 조용히, 그러나 꾸준히 성장합니다. 가끔 사람들은
+                ‘관념 자체를 전복시키는 아이디어를 내놓아야 한다’는 강박관념에
+                빠진 나머지, 자유로운 발상과 의견 개진에 있어 소극적일 때가
+                있습니다. 작은 발짝이 모여 큰 발전이 이루어지는 것이 아니라,
+                족적 하나를 남기더라도 거대한 형태로 모두를 놀라게 하고 싶은
+                욕망을 가지고 있기 때문입니다. 불세출의 천재가 아니고서야
+                하루아침에 높은 경지에 도달할 수는 없는 노릇이지만, 그런
+                하루하루의 노력이 쌓여 이루어지는 업적보다 ‘인생 한 방’이라는
+                꿈을 가지고, 필요 이상으로 두뇌 활동을 강박 관념으로 제어하는
+                사람들을 보면 안타깝다는 생각이 듭니다. 빛나는 원석을 숨기고
+                있을 수도 있는 사소한 아이디어를 중시하지 않고, 거대한
+                스케일이나 놀라운 발상을 가지고 있어야만 한다고 생각하며 그
+                자리를 고수하다가, 결국 몇 발짝 내딛지도 못하고 그 자리에서
+                발전하지 못하는 사람들을 저는 숱하게 봤기 때문입니다. 올바른
+                발전을 위해서는, 아주 미세한 발걸음이나 때때로 뒷걸음치는 일도
+                부끄러워하지 않고, 자신을 움직이는 작은 아이디어 하나하나를
+                중시해야 한다고 생각합니다. 천재는 아주 드문 확률로 발생하고,
+                우리 대부분은 천재가 아닌 만큼, 작은 노력이 쌓여 완성되는 거대한
+                업적의 스케치를 그리는 마음으로, 가벼운 데생부터 시작할 필요가
+                있는 것입니다.
+              </p>
+            </div>
+          </div>
+          <div className="part3">
+            <img
+              src="https://images.unsplash.com/photo-1550592704-6c76defa9985?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt="공부이미지"
+            />
+            <div>
+              <h3 className="about__title">평생 배움을 멈추지 않는 자세</h3>
+              <p>
+                평생에 걸쳐 배움을 멈추지 않고자 하는 의지로, 항상 노력하는
+                사람이 되고 싶습니다. 발전 속도에 박차를 가하기 시작한 현대에
+                이르러, 우리는 근 1년 사이에도 얼마든지 바뀌는 현실을 피부로
+                느끼면서, 과거 ‘당연하게 여기고’ 살았던 많은 일이 역사의
+                뒤안길로 사라지는 광경을 목격하곤 합니다. 그리고 새로운 문물에
+                때때로 곤혹스러움을 느끼며, 적응하지 못하는 경향을 보여주기도
+                하는 것입니다. 이와 같은 적응력은 어릴수록 더욱 민감하고 반응이
+                빠르며, 나이를 먹을수록 관념이 굳어 반사 신경이 다소 느려지는
+                모습을 보여줍니다. 그러나 자신을 자꾸 뒤로 밀려나게 하는 현실이
+                ‘지나치게 빠르다’며 한탄하는 것이 아니라, 조금이라도 발을 맞춰
+                나가기 위해 항상 정진하는 자세로 임하지 않으면, 결국 그 차이를
+                극복할 수 없을 만큼 밀려나, 사람들의 기억에 잊히곤 하는 과거의
+                유물들과 같은 신세로 전락할 수밖에 없는 것입니다. 그래서 저는
+                항상 시대 변화에 민감하게 반응하며 호기심을 가지는 어린아이와
+                같이, 저의 촉각을 곤두세워 두고자 합니다.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <ContactCont />
     <FooterCont />
   </div>
@@ -22,3 +113,61 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+.about__inner {
+  > div {
+    margin-bottom: 500px;
+    font-family: var(--font-kor3);
+    line-height: 1.6;
+    color: var(--black);
+
+    .about__title {
+      font-size: 24px;
+      margin-bottom: 50px;
+      color: var(--black);
+    }
+    p {
+      text-align: justify;
+    }
+  }
+  .part1 {
+    margin-top: 200px;
+
+    img {
+      margin-bottom: 50px;
+      object-fit: cover;
+    }
+    p {
+      width: 65%;
+      font-size: 16px;
+    }
+  }
+  .part2 {
+    display: flex;
+    justify-content: space-between;
+
+    img {
+      width: 65%;
+      object-fit: cover;
+    }
+    div {
+      width: 30%;
+      font-size: 16px;
+    }
+  }
+  .part3 {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row-reverse;
+
+    img {
+      width: 65%;
+      object-fit: cover;
+    }
+    div {
+      width: 30%;
+      font-size: 16px;
+    }
+  }
+}
+</style>
